d19dfbe0cf1a61aaa928f2bef3115a6c1d30dcda
nitrate92 <46730268+nitrate92@users.noreply.github.com>
2023-07-12 07:00:16 -0400

    peeing discord's pants

--- help.js
+++ help.js
@@ -11,8 +11,12 @@ const http = require("http");
 const mime = require('mime');
 const host = require('./config.json').main.host;
 const port = require('./config.json').main.port;
-
 const funnyList = fs.readdirSync("./servedContent");
+
+function getLastModified(file) {
+    const { mtime, ctime } = fs.statSync(file)
+    return new Date(mtime).toUTCString()
+}
 
 const requestListener = function (req, res) {
     const fuckYou = funnyList[Math.floor(Math.random() * funnyList.length)];
@@ -21,11 +25,12 @@ const requestListener = function (req, res) {
 
     res.writeHead(200, {
         'Content-Type': mime.getType(fuckYou),
-        'X-Powered-By': 'potatoes',
-        'Cache-Control': 'no-cache, no-store, must-revalidate'
+        'Last-Modified': getLastModified(`./servedContent/${fuckYou}`),
+        'Cache-Control': 'no-cache',
+        'X-Powered-By': 'potatoes'
     });
     res.end(fuckYouImgData);
-    console.log(`Returned ${fuckYou} with Content-Type "${mime.getType(fuckYou)}" to ${req.socket.remoteAddress}`);
+    console.log(`Returned ${fuckYou}, Last-Modified "${getLastModified(`./servedContent/${fuckYou}`)}" with Content-Type "${mime.getType(fuckYou)}" to ${req.socket.remoteAddress}`);
 };
 
 const server = http.createServer(requestListener);
